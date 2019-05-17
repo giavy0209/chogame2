ed5dfc0cbb899a5f05172a975021c0448d7aaa41
giavy0209 <49144082+giavy0209@users.noreply.github.com>
2019-05-17 10:23:31 +0700

    a

--- js/main.js
+++ js/main.js
@@ -1,13 +1,33 @@
-// dropdown menu
-function showMenu() {
-    attMenu = document.getElementById('menu').hasAttribute('class');
-
-    if (attMenu == false) {
-        menu.setAttribute('class', 'active');
-    } else {
-        menu.removeAttribute('class', 'active')
-    }
+vid = document.getElementById('news-video');
+thumb = document.getElementById('thumb');
+newsImgHeight = document.getElementById('set-height');
+getHeight = document.getElementById('getHeight');
+function playVid(){
+thumb.style.opacity = "0";
+thumb.style.pointerEvents = "none"
+vid.play();
 }
+function pauseVid(){
+vid.pause();
+// thumb.style.opacity = "1";
+thumb.style.pointerEvents = "all";
+}
+
+function showMenu(){
+attMenu = document.getElementById('menu').hasAttribute('class');
+
+if(attMenu == false ){
+    menu.setAttribute('class', 'active');
+}else{
+    menu.removeAttribute('class', 'active')
+}
+}
+
+var getHeight = getHeight.offsetHeight + 'px';
+window.onload = function(){
+newsImgHeight.style.height = getHeight;
+}
+//================
 
 // rating star
 star = document.querySelectorAll('.rating .star');
@@ -88,8 +108,10 @@ function removeSubStar() {
     star[3].classList.remove('sub-active')
     star[4].classList.remove('sub-active')
 }
-//===================================
+
+//========================
 getImg = Array.from(document.querySelectorAll('.slide-product .product-img'));
+console.log(getImg);
 function changeImg1(){
     getImg[0].classList.add('active');
     getImg[1].classList.remove('active');
